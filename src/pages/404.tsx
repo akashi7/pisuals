e4cbf39683ff9d58ffc094835424dadfb64f35eb
akashi7 <christiannseko@gmail.com>
2024-10-21 11:51:16 +0200

    css

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout'
 const NotFound = () => {
   return (
     <Layout>
-      <div className='w-full h-screen grid place-items-center bg-red-400'>
+      <div className='w-full h-[100%] grid place-items-center'>
         <div className='text-center'>
           <h1 className='text-[160px] font-bold text_404'>404</h1>
           <p className='text-[16px] text-gray-500'>Page not found</p>
